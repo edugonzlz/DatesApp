1c735d91b5a91a9bc9ff4210b5f7b4b1d9e52cc8
Edu González <Edu@MacBook-Pro-de-Edu.local>
2015-05-08 11:56:21 +0200

    Initial Commit

---
+++ MatchedUp2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MatchedUp2
+//
+//  Created by Edu González on 8/5/15.
+//  Copyright (c) 2015 Edu González. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MatchedUp2/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  MatchedUp2
+//
+//  Created by Edu González on 8/5/15.
+//  Copyright (c) 2015 Edu González. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
